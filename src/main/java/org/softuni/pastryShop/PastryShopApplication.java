2435ec2432375a4048c15ff388a8ff8043771b5e
Ralitsa Ivanova <ralitsaandrianova0405@gmail.com>
2023-11-14 23:42:43 +0200

    first commit - committing all files to the repository

---
+++ src/main/java/org/softuni/pastryShop/PastryShopApplication.java
@@ -0,0 +1,13 @@
+package org.softuni.pastryShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastryShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastryShopApplication.class, args);
+	}
+
+}
