e230ce603519a6e07c7eaf3bb7d0b89b02dc812c
Ralitsa Ivanova <ralitsaandrianova0405@gmail.com>
2023-12-04 00:18:47 +0200

    Adding Admin menu, Adding Shopping Card, adding footer fragment

--- src/main/java/org/softuni/pastryShop/web/ProductAddController.java
+++ src/main/java/org/softuni/pastryShop/web/ProductAddController.java
@@ -9,4 +9,9 @@ public class ProductAddController {
     public String addProduct(){
         return "addProduct";
     }
+
+    @GetMapping("addCategory")
+    public String addCategory(){
+        return "addCategory";
+    }
 }
